714044f876596dace3fe91568bcc8282a66310a5
Elizabeth Lis <elislis7@ya.ru>
2023-07-27 17:28:42 +0300

    попытка редактировать mainApi

--- src/utils/Api/MainApi.js
+++ src/utils/Api/MainApi.js
@@ -1,5 +1,93 @@
 class MainApi {
   constructor(config) {
+    this._baseURL = config.baseURL;
+    this._headers = config.headers
+  }
+
+  _checkServerResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка в запросе: ${res.status}`)
+  }
+
+  register(userData) {
+    return fetch(`${this._baseURL}/signup`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(userData),
+    })
+    .then(res => this._checkServerResponse(res))
+  }
+
+  login(userData) {
+    return fetch(`${this._baseURL}/signin`, {
+      method: 'POST',
+      headers: this._headers,
+      credentials: 'include',
+      body: JSON.stringify(userData),
+    })
+    .then(res => this._checkServerResponse(res))
+  }
+
+  logOut() {
+    return fetch(`${this._baseURL}/signout`, {
+      method: 'GET',
+      credentials: 'include',
+    })
+    .then(res => this._checkServerResponse(res));
+  }
+
+  getUser() {
+    return fetch(`${this._baseURL}/users/me`, {
+      method: 'GET',
+      headers: this._headers,
+      credentials: 'include',
+    })
+    .then(res => this._checkServerResponse(res));
+  }
+
+  updateUser(userData) {
+    return fetch(`${this._baseURL}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      credentials: 'include',
+      body: JSON.stringify(userData)
+    })
+    .then(res => this._checkServerResponse(res));
+  }
+
+  addMovie(movieData) {
+    return fetch(`${this._baseURL}/movies`, {
+      method: 'POST',
+      headers: this._headers,
+      credentials: 'include',
+      body: JSON.stringify(movieData),
+    })
+    .then(res => this._checkServerResponse(res));
+  }
+
+  getSavedMovies() {
+    return fetch(`${this._baseURL}/movies`, {
+      method: 'GET',
+      headers: this._headers,
+      credentials: 'include',
+    })
+    .then(res => this._checkServerResponse(res));
+  }
+
+  deleteMovie(movieId) {
+    return fetch(`${this._baseURL}/movies/${movieId}`, {
+      method: 'DELETE',
+      headers: this._headers,
+      credentials: 'include',
+    })
+    .then(res => this._checkServerResponse(res));
+  }
+} 
+
+/* class MainApi {
+  constructor(config) {
     this._url = config.url;
     console.log(this._url)
   }
@@ -41,46 +129,46 @@ class MainApi {
 
   // получение инфо о пользователе
   getUser() {
-    return this._request('users/me', 'GET')
+    return this._request('/users/me', 'GET')
   }
 
   // редактирование инфо о пользователе
   updateUser(userData) {
-    return this._request('users/me', 'PATCH', userData)
+    return this._request('/users/me', 'PATCH', userData)
   }
 
   //получение списка сохраненых фильмов
   getSavedMovies() {
-    return this._request('movies', 'GET')
+    return this._request('/movies', 'GET')
   }
 
   //добавление фильма в сохраненки
   addMovie(movieData) {
-    return this._request('movies', 'POST', movieData)
+    return this._request('/movies', 'POST', movieData)
   }
 
   //удаление фильма из сохраненок
   deleteMovie(movieId) {
-    return this._request(`movies/${movieId}`, 'DELETE')
+    return this._request(`/movies/${movieId}`, 'DELETE')
   }
 
   // логирование пользователя
   login(authData) {
-    return this._request('signin', 'POST', authData)
+    return this._request('/signin', 'POST', authData)
   }
 
   // регистрация пользователя
   register(authData) {
-    return this._request('signup', 'POST', authData)
+    return this._request('/signup', 'POST', authData)
   }
 
   logOut() {
-    return this._request('signout', 'GET')
+    return this._request('/signout', 'GET')
   }
-}
+} */
 
 export const apiMain = new MainApi ({
-  url: "https://lis.movies-explorer.nomoreparties.sbs",
+  baseURL: "https://lis.movies-explorer.nomoreparties.sbs",
   headers: {
     "Content-Type": "application/json",
   }
